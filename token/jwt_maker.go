2cd3bf01f26a9ec9c4eb99a175286a601fa22a7d
Pawas Pandey <pawaspy2633@gmail.com>
2025-04-01 21:27:57 +0530

    Added the paseto auth file

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -15,15 +16,43 @@ type JWTMaker struct {
 
 func NewJWTMaker(secretKey string) (Maker, error) {
 	if len(secretKey) < minSecretSize {
-		return nil, fmt.Errorf("Invalid key size: %d", secretKey)
+		return nil, fmt.Errorf("Invalid key size: %v", secretKey)
 	}
 	return &JWTMaker{secretKey}, nil
 }
 
 func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
 
+	jwToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwToken.SignedString([]byte(maker.secretKey))
 }
 
 func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(maker.secretKey), nil
+	}
 
+	jwToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
 }
--- token/payload.go
+++ token/payload.go
@@ -1,10 +1,18 @@
 package token
 
 import (
+	"errors"
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 
 	"github.com/google/uuid"
 )
+
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+) 
 
 type Payload struct {
 	ID        uuid.UUID `json:"id"`
@@ -20,10 +28,47 @@ func NewPayload(username string, duration time.Duration) (*Payload, error) {
 	}
 
 	payload := &Payload{
-		ID: tokenId,
-		Username: username,
-		IssuedAt: time.Now(),
+		ID:        tokenId,
+		Username:  username,
+		IssuedAt:  time.Now(),
 		ExpiredAt: time.Now().Add(duration),
 	}
 	return payload, nil
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
+
+func (payload *Payload) GetExpirationTime() (*jwt.NumericDate, error) {
+	return &jwt.NumericDate{
+		Time: payload.ExpiredAt,
+	}, nil
+}
+
+func (payload *Payload) GetIssuedAt() (*jwt.NumericDate, error) {
+	return &jwt.NumericDate{
+		Time: payload.IssuedAt,
+	}, nil
+}
+
+func (payload *Payload) GetNotBefore() (*jwt.NumericDate, error) {
+	return &jwt.NumericDate{
+		Time: payload.IssuedAt,
+	}, nil
+}
+
+func (payload *Payload) GetIssuer() (string, error) {
+	return "", nil
+}
+
+func (payload *Payload) GetSubject() (string, error){
+	return "", nil
+}
+
+func (payload *Payload) GetAudience() (jwt.ClaimStrings, error){
+	return jwt.ClaimStrings{}, nil
 }
