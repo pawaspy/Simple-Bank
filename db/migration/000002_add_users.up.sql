f89417f7b90b19d723fc74ae3fd009eedde50c30
Pawas Pandey <pawaspy2633@gmail.com>
2025-04-01 13:05:55 +0530

    Updated the sql migration

--- db/migration/000002_add_users.up.sql
+++ db/migration/000002_add_users.up.sql
@@ -7,8 +7,6 @@ CREATE TABLE "users" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
--- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
-
 ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
 ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
